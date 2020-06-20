791cb611b2f4e23b1a5ab871031683e3fa7a024d
Davy de Haas <davy.dehaas98@gmail.com>
2020-06-20 14:35:33 +0200

    DWET-77: Add base classes, install NuGet dependencies

---
+++ ProfileMicroservice/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProfileMicroservice.Entities
+{
+    public abstract class BaseEntity
+    {
+        [BsonId] public Guid Id { get; set; }
+    }
+}
